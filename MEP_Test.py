28781b288286b703d6c6fa96acc8c37a5d627eac
Birdboys <colbyjbird@comcast.net>
2021-09-13 08:58:34 -0600

    added electrical section

--- MEP_Test.py
+++ MEP_Test.py
@@ -10,6 +10,7 @@ def powerset(iterable):
 app = Flask(__name__)
 app.secret_key = 'bigchungus'
 rules = pandas.read_csv("mil_rules.csv", header=0)
+rules_electrical = pandas.read_csv("mil_rules_electrical.csv", header=0)
 
 @app.route("/", methods = ["POST","GET"])
 def home():
@@ -67,5 +68,60 @@ def home():
     else:
         return render_template("item_select.html", i = "")
 
+@app.route("/electrical", methods = ["POST","GET"])
+def electrical():
+    if request.method == "POST":
+        items = request.form.get("item")
+        added = request.form.get("add_quote")
+        re = request.form.get("reset")
+        rec = request.form.get("rec")
+        
+        if rec:
+            if rec not in session['item_electrical']:
+                temp = session['item_electrical']
+                temp.append(rec.replace("~"," "))
+                session['item_electrical'] = temp
+            sets = list(powerset(session['item_electrical'][-4:]))
+            sets = list(map(set, sets))
+            cons = []
+
+            for index, row in rules_electrical.iterrows():
+                if set(ast.literal_eval(row['antecedent'])) in sets and ast.literal_eval(row['consequent']) not in cons and ast.literal_eval(row['consequent'])[0] not in session['item_electrical']:
+                    cons.append(ast.literal_eval(row['consequent']))
+                if len(cons) > 2:
+                    break;
+            cons_stripped = []
+            for string in cons:
+                cons_stripped.append(string[0].replace(" ","~"))
+            return render_template("electric_item_select.html",  i = session['item_electrical'], last_items = cons, last_items_stripped = cons_stripped)
+        
+        if added and items:
+            if 'item_electrical' in session:
+                if items not in session['item_electrical']:
+                    temp = session['item_electrical']
+                    temp.append(items)
+                    session['item_electrical'] = temp
+            else:
+                session['item_electrical'] = [items]
+            sets = list(powerset(session['item_electrical'][-4:]))
+            sets = list(map(set, sets))
+            cons = []
+
+            for index, row in rules_electrical.iterrows():
+                if set(ast.literal_eval(row['antecedent'])) in sets and ast.literal_eval(row['consequent']) not in cons and ast.literal_eval(row['consequent'])[0] not in session['item_electrical']:
+                    cons.append(ast.literal_eval(row['consequent']))
+                if len(cons) > 2:
+                    break;
+            cons_stripped = []
+            for string in cons:
+                cons_stripped.append(string[0].replace(" ","~"))
+            return render_template("electric_item_select.html",  i = session['item_electrical'], last_items = cons, last_items_stripped = cons_stripped)
+        elif re:
+            session.pop('item_electrical', None)
+            return render_template("electric_item_select.html", i = "")
+        else:
+            return render_template("electric_item_select.html", i = "")
+    else:
+        return render_template("electric_item_select.html")
 if __name__ == "__main__":
     app.run(debug = True)
--- datalist_Gen.py
+++ datalist_Gen.py
@@ -1,9 +1,10 @@
 import pandas as pd 
 import csv
 
-with open('mil_items.csv', 'rt')as f:
-        with open('mil_items.txt', 'w+') as f2:
+with open('mil_items_electrical.csv', 'rt')as f:
+        with open('mil_items_electrical.txt', 'w+') as f2:
             for line in f:
                 f2.write(f"<option value =\"{line.rstrip()}\"> \n")
                         
 f2.close()
+
